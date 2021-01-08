36d3fcb3631611888f64248dab7c4ab9e202d33b
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-01-07 22:04:54 -0700

    added connection, links

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,10 +8,11 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/index.js",
   "/manifest.webmanifest",
-  "/style.css",
+  "/styles.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
+
 
 // install
 self.addEventListener("install", function(evt) {
